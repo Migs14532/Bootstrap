4892d87725e3a69e70d12a1add51aec233ffba52
Migs14532 <miguelpaolobarafon.202301454@gmail.com>
2024-10-24 06:52:11 +0000

    update

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 (function () {
-  emailjs.init("ekpgtPW3xJLqbhy-X");
-  console.log("EmailJS initialized");
-})();
-
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log("Form submitted");
-
-    emailjs.sendForm("service_j7zvhr1", "template_mhz2yqj", this).then(
-      function () {
-        console.log("Email sent successfully");
-        alert("Message Sent Successfully!");
-      },
-      function (error) {
-        console.error("Error sending email:", error);
-        alert("Failed to send the message: " + JSON.stringify(error));
-      }
-    );
-  });
+    emailjs.init("ekpgtPW3xJLqbhy-X");
+    console.log("EmailJS initialized");
+  })();
+  
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      console.log("Form submitted");
+  
+      emailjs.sendForm("service_q0ph04k", "template_3bs11sn", this).then(
+        function () {
+          console.log("Email sent successfully");
+          alert("Message Sent Successfully!");
+        },
+        function (error) {
+          console.error("Error sending email:", error);
+          alert("Failed to send the message: " + JSON.stringify(error));
+        }
+      );
+    });
